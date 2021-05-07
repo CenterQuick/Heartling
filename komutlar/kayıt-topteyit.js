8503f14324ee888c45d957faab81a7f037223666
CenterQuick <75475710+CenterQuick@users.noreply.github.com>
2021-05-07 11:53:33 +0300

    Update kayıt-topteyit.js

--- komutlar/kayıt-topteyit.js
+++ komutlar/kayıt-topteyit.js
@@ -3,7 +3,7 @@ const db = require('quick.db')
 
 exports.run = async (client, message, member) => {
   
-if(!message.member.roles.cache.some(r => ['796056229797429268', '796056220045541437', "796056210537054229"].includes(r.id)) && (!message.member.hasPermission("ADMINISTRATOR")))return message.reply("Bu Komutu Kullanmak İçin Yetkiniz Bulunmamakta.")  
+if(!message.member.roles.cache.some(r => ['840136943911305247'].includes(r.id)) && (!message.member.hasPermission("ADMINISTRATOR")))return message.reply("Bu Komutu Kullanmak İçin Yetkiniz Bulunmamakta.")  
 
   let uye = message.mentions.users.first() || message.author;
 let bilgi = db.get(`yetkili.${uye.id}.toplam`);

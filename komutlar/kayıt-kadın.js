7aebb5a06e37db9b532408708ce6ca843af1abb5
CenterQuick <75475710+CenterQuick@users.noreply.github.com>
2021-05-07 11:49:07 +0300

    Update kayıt-kadın.js

--- komutlar/kayıt-kadın.js
+++ komutlar/kayıt-kadın.js
@@ -3,12 +3,12 @@ const db = require('quick.db')
 
 exports.run = async (client, message, args) => {
   
-if(!["796056210537054229", "796056220045541437", "796056229797429268"].some(role => message.member.roles.cache.get(role)) && (!message.member.hasPermission("ADMINISTRATOR"))) return message.channel.send(`Bu Komutu Kullanabilmek İçin Yetkin Bulunmuyor.`)
+if(!["840137761793245184"].some(role => message.member.roles.cache.get(role)) && (!message.member.hasPermission("ADMINISTRATOR"))) return message.channel.send(`Bu Komutu Kullanabilmek İçin Yetkin Bulunmuyor.`)
   
-const kadin = message.guild.roles.cache.find(r => r.id === "795581186557870090")
-const kayıtsız = message.guild.roles.cache.find(r => r.id === "796042512661479454")
-const reglog = message.guild.channels.cache.find(c => c.id === "796043030901555251")
-const genelchat = message.guild.channels.cache.find(g => g.id === "795575439212281881")
+const kadin = message.guild.roles.cache.find(r => r.id === "840137268182515722")
+const kayıtsız = message.guild.roles.cache.find(r => r.id === "840137305607634995")
+const reglog = message.guild.channels.cache.find(c => c.id === "840144794022051840")
+const genelchat = message.guild.channels.cache.find(g => g.id === "840146116829773875")
 
 const member = message.guild.member(message.mentions.members.first() || message.guild.members.cache.get(args[0]));
 if(!member) return message.channel.send(`Bir Kullanıcı Belirt.`)
@@ -17,9 +17,7 @@ const x = message.guild.member(member)
 
 let tag = "ΛV"
 let isim = args[1]
-let yas = Number(args[2])
 if(!isim) return message.channel.send(`Bir İsim Belirt`)
-if(!yas) return message.channel.send(`Bir Yaş Belirt`)
 
 let bilgi = db.get(`yetkili.${member.id}`);  
 db.add(`yetkili.${message.author.id}.kadin`,1 )
@@ -27,11 +25,11 @@ db.add(`yetkili.${message.author.id}.toplam`, 1)
 let toplami = db.fetch(`yetkili.${message.author.id}.toplam`)  
 
 message.react('✅')
-x.setNickname(`${tag} ${isim} • ${yas}`)
+x.setNickname(`${tag} • ${isim}`)
 x.roles.add(kadin)
 x.roles.remove(kayıtsız)
 //
-x.setNickname(`${tag} ${isim} • ${yas}`)
+x.setNickname(`${tag} • ${isim}`)
 x.roles.add(kadin)
 x.roles.remove(kayıtsız)
 
@@ -46,26 +44,26 @@ const embed = new Discord.MessageEmbed()
     .addField(`Kayıt Edilen:`, `<@${member.id}> Kayıt Oldu`)
     .addField(`Verilen Rol:`, `<@&${kadin.id}> Rolleri Verildi`) 
     .addField(`Alınan Rol:`, `<@&${kayıtsız.id}> Rolleri Alındı`)
-    .addField(`Yeni İsmin:`, `\`${tag} ${isim} | ${yas}\` Olarak Güncellendi`) 
+    .addField(`Yeni İsmin:`, `\`${tag} • ${isim}\` Olarak Güncellendi`) 
     .addField(`Yetkili Toplam:`, `\`${kayıtlar}\` Kayıtlara Sahip.`)
     .setThumbnail(message.author.avatarURL())
-.setFooter(`Heartling Register`)
+.setFooter(`NightMare Register`)
 .setColor('GREEN')
-client.channels.cache.get('795575439212281881').send(embed)
+client.channels.cache.get('840146116829773875').send(embed)
 
 
-genelchat.send(`<@${member.id}>, Aramıza Hoş Geldin ! Umarım Keyifli Vakitler Geçirirsin.`)
+genelchat.send(`<@${member.id}>, NightMare sunucusuna Hoş Geldin. Keyifli zaman geçirmeni dileriz.`)
 
 }
 
 exports.conf = {
     enabled: true,
     guildOnly: false,
-    aliases: ["kadın", "k", "woman", "girl", "kız"],
+    aliases: ["register", "k", "kayit", "reg"],
     permLevel: 0
 };
 
 exports.help = {
-    name: "kadın"
+    name: "kayıt"
 }
 
